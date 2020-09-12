f681392853906a326a7cfc7e2c1128f19cd23203
peter <perter@jinghang.com>
2020-09-12 11:56:08 +0800

    冲突解决，提交到本地库!

--- src/com/beicai/Test01.java
+++ src/com/beicai/Test01.java
@@ -1,11 +1,10 @@
 package com.beicai;
 
-public class Main {
+public class Test01 {
 
     public static void main(String[] args) {
 
         System.out.println("初始化项目！！！！");
-        // hotfix 分支上做出了修改！！！
+        // hotfix 分支上做出了修改！！！ 并且合并上来！
     }
 }
-
