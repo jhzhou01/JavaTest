e7b2a4b82b8880ea92b563d9884b5e9e4fa3adc3
peter <perter@jinghang.com>
2020-09-12 11:53:24 +0800

    master 开发了一个test02!

---
+++ src/com/test/Test02.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @Classname Test02
+ * @Date 2020/9/12 11:50
+ * @Created by 周飞
+ */
+public class Test02 {
+
+    public static void main(String[] args) {
+        System.out.println("开发了一个类test02");
+    }
+}
